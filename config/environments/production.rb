245e664e36ae65d54c58a6e24ea81b3934ded4c6
Shylah Barnes <shylah.barnes@gmail.com>
2016-04-13 06:13:31 -0700

    Changed production.rb to more true values to help Heroku recognize CSS code.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true#false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  config.assets.compile = false  #CHANGED FOR HEROKU -S.B., 04-12-16
+  config.assets.compile = true  #CHANGED FOR HEROKU -S.B., 04-12-16
 
   # Generate digests for assets URLs.
   config.assets.digest = true
